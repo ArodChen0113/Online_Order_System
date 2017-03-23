0f318e9d1553e15c010f0ed4fb2dd67f6b33c138
Arod Chen <paul3264119@yahoo.com.tw>
2017-03-23 12:57:54 +0800

    路由測試(刪除不需要的檔案)

--- model/dbConfigM.php
+++
@@ -1,7 +0,0 @@
-<?php
-// 資料庫連接參數
-global $_DB;
-$_DB['host'] = "127.0.0.1";
-$_DB['username'] = "root";
-$_DB['password'] = "root";
-$_DB['dbname'] = "test";
--- route.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: arodchen
- * Date: 2017/3/22
- * Time: 下午 4:47
- */
